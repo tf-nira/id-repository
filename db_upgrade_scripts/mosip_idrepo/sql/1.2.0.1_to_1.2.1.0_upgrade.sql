2319b4b9af40a3b776bc6912081f8522854a6644
Sowmya U B <sowmya.ub@technoforte.co.in>
2024-09-12 11:22:51 +0530

    added upgrade and roolback scripts
    Signed-off-by: Sowmya U B <sowmya.ub@technoforte.co.in>

--- db_upgrade_scripts/mosip_idrepo/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
+++ db_upgrade_scripts/mosip_idrepo/sql/1.2.0.1_to_1.2.1.0_upgrade.sql
@@ -1,1 +1,18 @@
-\echo 'Upgrade Queries not required for transition from $CURRENT_VERSION to $UPGRADE_VERSION'
+\c mosip_idrepo sysadmin
+---------------------------------------------------------------------------------------------------
+
+ALTER TABLE idrepo.uin ADD COLUMN part1 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin ADD COLUMN part2 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin ADD COLUMN part3 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin ADD COLUMN part4 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin_h ADD COLUMN part1 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin_h ADD COLUMN part2 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin_h ADD COLUMN part3 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
+
+ALTER TABLE idrepo.uin_h ADD COLUMN part4 character varying(64) DEFAULT 'ZmFsc2U=' NOT NULL;
