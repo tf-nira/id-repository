830f292268377f9264254890ce4b05d6749e8e40
Soumya Raj <soumya.raj@technoforte.co.in>
2025-07-02 14:43:04 +0530

    changed logic for calling updateJsonObject and spouseDetailsHelper methods

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoServiceImpl.java
@@ -546,23 +546,17 @@ public class IdRepoServiceImpl implements IdRepoService<IdRequestDTO, Uin> {
 				DocumentContext dbData = JsonPath.using(configuration).parse(new String(uinObject.getUinData()));
 				anonymousProfileHelper.setOldUinData(dbData.jsonString().getBytes());
 				updateVerifiedAttributes(requestDTO, inputData, dbData);
-				JSONCompareResult comparisonResult = JSONCompare.compareJSON(inputData.jsonString(),
-						dbData.jsonString(), JSONCompareMode.LENIENT);
-				String numberOfOtherSpousesInput = spouseDetailHelper.getStringData(
-						idRepoServiceHelper.getMappingJsonValue("numberOfOtherSpouses"), inputData, null, false);
-				mosipLogger.info("numberOfOtherSpousesInput : " + numberOfOtherSpousesInput);
-				if (comparisonResult.failed() && numberOfOtherSpousesInput == null) {
-					updateJsonObject(uinHash, inputData, dbData, comparisonResult, true);
-				}
-				String numberOfOtherSpousesInputNew = spouseDetailHelper.getStringData(
-						idRepoServiceHelper.getMappingJsonValue("numberOfOtherSpouses"), inputData, null, false);
-				mosipLogger.info("numberOfOtherSpousesInputNew : " + numberOfOtherSpousesInputNew);
 
-				boolean isAddSpouse = Objects.equals(spouseDetailHelper.getStringData("addSpouse", inputData, null, false), "Y");
-				boolean isRemoveSpouse = Objects.equals(spouseDetailHelper.getStringData("removeSpouse", inputData, null, false), "Y");
+				boolean isAddSpouse = Objects.equals(spouseDetailHelper.getStringData(idRepoServiceHelper.getMappingJsonValue("addSpouse"), inputData, null, false), "Y");
+				boolean isRemoveSpouse = Objects.equals(spouseDetailHelper.getStringData(idRepoServiceHelper.getMappingJsonValue("removeSpouse"), inputData, null, false), "Y");
 
 				if (isAddSpouse) spouseDetailHelper.addSpouseDetails(inputData, dbData);
 				if (isRemoveSpouse) spouseDetailHelper.updateSpouseDetails(requestDTO, inputData, dbData);
+
+				JSONCompareResult comparisonResult = JSONCompare.compareJSON(inputData.jsonString(), dbData.jsonString(), JSONCompareMode.LENIENT);
+				if (comparisonResult.failed()) {
+					updateJsonObject(uinHash, inputData, dbData, comparisonResult, true);
+				}
 
 				uinObject.setUinData(convertToBytes(convertToObject(dbData.jsonString().getBytes(), Map.class)));
 				uinObject.setUinDataHash(securityManager.hash(uinObject.getUinData()));
