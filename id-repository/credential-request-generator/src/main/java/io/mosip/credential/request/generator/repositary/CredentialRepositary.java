cc50ba7ac128bcbcdc617e3437e342fcc44a53f2
Manishch22 <126442522+Manishch22@users.noreply.github.com>
2025-08-19 18:58:31 +0530

    updated query for credential batch job

--- id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
+++ id-repository/credential-request-generator/src/main/java/io/mosip/credential/request/generator/repositary/CredentialRepositary.java
@@ -44,9 +44,11 @@ public interface CredentialRepositary<T extends CredentialEntity, E> extends Bas
 			Pageable pageable);
 	
 	@Transactional
-	@Lock(value = LockModeType.PESSIMISTIC_WRITE) 
-	@QueryHints({ @QueryHint(name = "javax.persistence.lock.timeout", value = "1") })
-	@Query("select c from CredentialEntity c where c.statusCode=:statusCode")
+//	@Lock(value = LockModeType.PESSIMISTIC_WRITE) 
+//	@QueryHints({ @QueryHint(name = "javax.persistence.lock.timeout", value = "1") })
+//	@Query("select c from CredentialEntity c where c.statusCode=:statusCode")
+	@Query(value = "SELECT * FROM credential_transaction c WHERE c.status_code = :statusCode ORDER BY c.cr_dtimes FOR UPDATE SKIP LOCKED",
+		countQuery = "SELECT count(*) FROM credential_transaction c WHERE c.status_code = :statusCode", nativeQuery = true)
 	Page<CredentialEntity> findCredentialByStatusCode(@Param("statusCode")String statusCode, Pageable pageable);
 
 	/**
