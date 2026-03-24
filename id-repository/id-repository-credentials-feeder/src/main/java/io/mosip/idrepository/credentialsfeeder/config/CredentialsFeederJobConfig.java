a2d4612105283f953ffb8ae0a549c90aefa5465a
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-03-24 13:43:16 +0530

    Updated the to and from date for credential-feeder instance 3.

--- id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/config/CredentialsFeederJobConfig.java
+++ id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/config/CredentialsFeederJobConfig.java
@@ -60,10 +60,10 @@ public class CredentialsFeederJobConfig {
 	@Value("${" + MOSIP_IDREPO_IDENTITY_UIN_STATUS_REGISTERED + "}")
 	private String uinActiveStatus;
 
-	@Value("${idrepo.credential.feeder.instance2.from-date}")
+	@Value("${idrepo.credential.feeder.instance3.from-date}")
 	private String fromDateStr;
 	
-	@Value("${idrepo.credential.feeder.instance2.to-date:}")
+	@Value("${idrepo.credential.feeder.instance3.to-date:}")
 	private String toDateStr;
 	
 	/**
