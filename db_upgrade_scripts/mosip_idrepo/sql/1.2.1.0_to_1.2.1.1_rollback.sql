03570275e3a86ca7bd1f6af4e48fbf18bf86d4e2
Soumya Raj <soumya.raj@technoforte.co.in>
2026-06-05 11:32:00 +0530

    Add 'remark' column to idrepo.uin and idrepo.uin_h tables for Alien Cancellation packets

---
+++ db_upgrade_scripts/mosip_idrepo/sql/1.2.1.0_to_1.2.1.1_rollback.sql
@@ -0,0 +1,4 @@
+-- Rollback: Remove 'remark' column from idrepo.uin and idrepo.uin_h tables.
+
+ALTER TABLE idrepo.uin DROP COLUMN IF EXISTS remark;
+ALTER TABLE idrepo.uin_h DROP COLUMN IF EXISTS remark;
