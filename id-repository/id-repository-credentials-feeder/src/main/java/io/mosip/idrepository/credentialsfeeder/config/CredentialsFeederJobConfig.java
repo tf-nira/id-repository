1194fc9057d628135ad1d8f3bceb203fa61dde0e
malaypandey-technoforte <malay.pandey@technoforte.co.in>
2026-06-26 08:53:54 +0530

    Added default value

--- id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/config/CredentialsFeederJobConfig.java
+++ id-repository/id-repository-credentials-feeder/src/main/java/io/mosip/idrepository/credentialsfeeder/config/CredentialsFeederJobConfig.java
@@ -62,7 +62,7 @@ public class CredentialsFeederJobConfig {
 	@Value("${" + MOSIP_IDREPO_IDENTITY_UIN_STATUS_REGISTERED + "}")
 	private String uinActiveStatus;
 
-	@Value("${idrepo.credential.feeder.instance-id6}")
+	@Value("${idrepo.credential.feeder.instance-id6:instance6}")
 	String instanceId;
 	
 	/**
